5b57ba11e792e59d85bfdf0ff175f117fd0fef53
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-01-04 23:42:46 -0800

    remove print

--- tests/aggregations/test_estimated_income_after_tax.py
+++ tests/aggregations/test_estimated_income_after_tax.py
@@ -9,6 +9,5 @@ def test_estimated_income_after_tax():
     data = sample_data()
 
     income = estimated_income_after_tax(data, monthly_income=7750)
-    print(income)
 
     assert np.isclose(income, 9707.65, atol=200)
