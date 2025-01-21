a42f5f5a0b2736eef56c0a5c613938da98e3da52
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-01-20 20:16:45 -0800

    giving up

--- src/visualize/yearly/sankey_flow.py
+++ src/visualize/yearly/sankey_flow.py
@@ -1,19 +1,58 @@
 import pandas as pd
 from os.path import join
-
 from sankeyflow import Sankey
 import matplotlib.pyplot as plt
 
-from src.visualize.yearly._sankey_helpers import sankey_inputs
+from typing import List, NamedTuple
+from numbers import Number
+from itertools import chain
+
 from src.aggregations.estimated_income_after_tax import estimated_income_after_tax
 from src.utilities.read_data import read_data
 from src.utilities.paths import untracked_path, get_year
 from src.utilities.column import Column
 from src.utilities.dictionary_ops import (
     NestedDict,
+    dictionary_sum,
 )
 from src.read_config.config_globals import config_globals
 
+class Flow(NamedTuple):
+    """
+    A flow connecting two nodes.
+
+    Attributes:
+        source (str): the source of the flow
+        target (str): the destination of the flow
+        flow_size (Number): how much money is flowing
+        options (dict): optional dictionary passed to Sankey
+    """
+
+    source: str
+    target: str
+    flow_size: Number
+    options: dict = {}
+
+def _get_flows(source: str, d: NestedDict) -> List[Flow]:
+    """
+    Returns all the flows in this subdictionary.
+    """
+    if not isinstance(d, dict): return []
+    
+    if source == "Income":
+        options = {"flow_color_mode": "source"}
+    else:
+        options = {}
+
+    this_layer = [
+        Flow(source=source, target=label, flow_size=dictionary_sum(branch), options=options)
+        for label, branch in d.items()
+    ]
+
+    this_layer.extend(
+        chain(*[_get_flows(label, branch) for label, branch in d.items()])
+    )
+    return this_layer
 
 def sankey_flow(df: pd.DataFrame, out_dir: str):
     """
@@ -30,11 +69,9 @@ def sankey_flow(df: pd.DataFrame, out_dir: str):
     total_spent = df[Column.PRICE].sum()
 
     flow: NestedDict = {
-        "Income": {
-            "Saved": estimated_income_after_tax(df) - total_spent,
-            "Controllable": {"Other": 0},
-            "Not Controllable": {"Food": {}, "Other": 0},
-        }
+        "Saved": estimated_income_after_tax(df) - total_spent,
+        "Controllable": {"Other": 0},
+        "Not Controllable": {"Food": {}, "Other": 0},
     }
 
     cats = df.groupby([Column.CATEGORY])[Column.CONTROLLABLE].mean()
@@ -52,24 +89,21 @@ def sankey_flow(df: pd.DataFrame, out_dir: str):
         )
 
         if round(this_cat[Column.IS_FOOD].mean()) == 1:
-            flow["Income"]["Not Controllable"]["Food"][cat_t] = (
-                flow["Income"]["Not Controllable"]["Food"].get(cat_t, 0) + cat_spent
+            flow["Not Controllable"]["Food"][cat_t] = (
+                flow["Not Controllable"]["Food"].get(cat_t, 0) + cat_spent
             )
 
         else:
-            flow["Income"][control_key][cat_t] = (
-                flow["Income"][control_key].get(cat_t, 0) + cat_spent
+            flow[control_key][cat_t] = (
+                flow[control_key].get(cat_t, 0) + cat_spent
             )
-
-    nodes, flows = sankey_inputs(flow)
 
     plt.clf()
     plt.figure(figsize=(15, 8))
     plt.title(f"Spending Flow for {get_year()}")
 
     s = Sankey(
-        flows=flows,
-        nodes=nodes,
+        flows=_get_flows("Income", flow),
     )
     s.draw()
     plt.savefig(join(out_dir, "sankey.png"))
