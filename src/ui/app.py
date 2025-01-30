f7ae00d64c5266311ce76e19baf789f280e23951
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-01-29 20:32:41 -0800

    html bones

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-from pathlib import Path
-from os.path import join
 
-TITLE = "Fauxduciary"
-ROOT = str(Path(__file__).parent)
+SITE_TITLE = "Fauxduciary"
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home() -> str:
-    return render_template("home.html", title=TITLE)
+    return render_template("home.html", site_title=SITE_TITLE)
+
+@app.route("/config")
+def config() -> str:
+    plots = []
+    aggs = []
+    return render_template("config.html", plots=plots, aggs=aggs)
