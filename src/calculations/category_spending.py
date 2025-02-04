9bb754ee72ab1909fa0a4e52854f8ecb16403cfe
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-02-03 21:39:42 -0800

    removing income kwargs (#20)

--- src/calculations/category_spending.py
+++ src/calculations/category_spending.py
@@ -1,22 +1,17 @@
 import pandas as pd
 from typing import Dict
-from src.utilities.types import Number
 
 from src.utilities.helpers import monthly_income
 from src.utilities.column import Column
 
 
-def category_spending(
-    df: pd.DataFrame, income: Number = monthly_income()
-) -> Dict[str, float]:
+def category_spending(df: pd.DataFrame) -> Dict[str, float]:
     """
     Calculates how much was spent on each category, as well as how much was
     spent in total and how much was made in that period.
 
     Parameters:
         df (DataFrame): the Pandas DataFrame to analyze
-        income (Optional[Number]): the monthly income to compare to. Defaults
-            to `helpers.monthly_income()`
 
     Returns:
         categories (Dict[str, float]): mapping from category name to how much
@@ -26,7 +21,7 @@ def category_spending(
 
     # we add one so the range is inclusive of both ends
     num_days = (df[Column.DATE].max() - df[Column.DATE].min()).days + 1
-    prorated_income = num_days * income * 12 / 365
+    prorated_income = num_days * monthly_income() * 12 / 365
 
     x = list(totals.index) + ["Total spent", "Income"]
     y = list(totals.values) + [df[Column.PRICE].sum(), prorated_income]
