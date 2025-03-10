4f946880b3e50350afe06a1ee2658e8491b1ee86
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-03-09 19:30:01 -0700

    expanding ui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
-from src.analyze_spending import AnalyzeSpending
+from src.analyze_spending import analyze_spending
+from src.drivers.ui.ui_driver import UIDriver
 from src.initialize import initialize
 from src.utilities.parse_args import parse_args, Subcommand
 
@@ -9,8 +10,8 @@ if __name__ == "__main__":
     if cmd == Subcommand.INIT:
         initialize()
     elif cmd == Subcommand.CLI:
-        AnalyzeSpending.analyze_spending()
+        analyze_spending()
     elif cmd == Subcommand.UI:
-        AnalyzeSpending().mainloop()
+        UIDriver().mainloop()
     else:
         raise ValueError(f"Invalid subcommand {sys.argv[1]}")
