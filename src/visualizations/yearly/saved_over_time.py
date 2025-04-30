cc8e233595292c80c9a855205f25e1f52d650ea5
Michael Veillon <49701743+mveillon@users.noreply.github.com>
2025-04-29 19:34:40 -0700

    calculating total saved per day

--- src/visualizations/yearly/saved_over_time.py
+++ src/visualizations/yearly/saved_over_time.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from os.path import join
-from typing import cast, List
+from typing import Dict, Tuple
+from datetime import timedelta, datetime
 
 from src.models.day_counts import DayCounts
 from src.utilities.helpers import monthly_income, get_weeks
@@ -22,48 +23,36 @@ def saved_over_time(df: pd.DataFrame, out_dir: str) -> None:
     Returns:
         None
     """
-    fmt = "%b"
-    payments = get_weeks(df[Column.DATE].min(), df[Column.DATE].max())
-    dates = []
-    balance_changes = []
-    expected_saved = [0.0]
-    for dtm in payments:
-        dates.append(dtm)
-        made = monthly_income() / DayCounts.weeks_per_month()
-        balance_changes.append(made)
-        expected_saved.append(expected_saved[-1] + (made * 0.2))
+    fmt = "%m/%d/%Y"
 
-    expected_saved = expected_saved[1:]
+    spent_by_date: Dict[str, float] = {}
+    current = df[Column.DATE].min()
+    end_date = df[Column.DATE].max()
+    per_day = monthly_income() / DayCounts.days_per_month()
+    while current <= end_date:
+        spent_by_date[current.strftime(fmt)] = per_day
+        current += timedelta(days=1)
 
     spending = df.groupby(Column.DATE)[Column.PRICE].sum()
-    for date, spent in spending.to_dict().items():
-        dates.append(date)
-        balance_changes.append(-spent)
+    for dtm, spent in spending.to_dict().items():
+        spent_by_date[dtm.strftime(fmt)] -= spent
 
-    inds = sorted(range(len(dates)), key=dates.__getitem__)
-    x = np.array(dates)[inds]
-    sorted_changes = np.array(balance_changes)[inds]
-    balance = [0.0]
-    for change in sorted_changes:
-        balance.append(balance[-1] + change)
+    all_dates = np.array(list(spent_by_date.keys()))
+    inds = sorted(range(all_dates.shape[0]), key=all_dates.__getitem__)
+    x = all_dates[inds]
+    y = np.cumsum(np.array(list(spent_by_date.values()))[inds])
 
-    y = np.array(balance)[1:]
-
-    days_of_year = np.fromiter(
-        map(lambda d: int(d.strftime("%j")), x),
-        dtype=int,
-        count=x.shape[0],
-    )
+    days_of_year = np.arange(y.shape[0], dtype=int)
     poly_model = np.polynomial.Polynomial.fit(days_of_year, y, 3)
     trend = poly_model(days_of_year)
 
     plt.clf()
     plt.title("Total Saved over Time")
     plt.ylabel("Total Income Added")
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(fmt))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b"))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(bymonthday=[1]))
     plt.plot(x, y, "b", label="Saved")
-    plt.plot(cast(List[int], payments), expected_saved, "g", label="Goal")
+    plt.plot(x, np.cumsum(np.full(y.shape, per_day * 0.2)), "g", label="Goal")
     plt.plot(x, trend, "--b", label="trend")
     plt.legend()
 
